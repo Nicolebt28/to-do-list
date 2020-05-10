ca3e1528965c8773bd2957f098c66fbb9e7c7803
Nicolebt28 <51862876+Nicolebt28@users.noreply.github.com>
2020-05-10 14:42:11 -0400

    Only text needs check (#9)
    * checking for unchecked
    
    * fixing the need to have all checked
    
    * only need check if text

--- check.js
+++ check.js
@@ -1,29 +1,26 @@
 document.getElementById("submit").onclick = function checkTheList () {
-  if ((document.getElementsByClassName('checklist__to-do-item')[0] && document.getElementsByClassName('checklist__box')[0].checked) === false){
+  if (((document.getElementsByClassName('checklist__to-do-item')[0].value) !== '') && ((document.getElementsByClassName('checklist__box')[0].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[1].value && document.getElementsByClassName('checklist__box')[1].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[1].value) !== '') && ((document.getElementsByClassName('checklist__box')[1].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[2].value && document.getElementsByClassName('checklist__box')[2].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[2].value) !== '') && ((document.getElementsByClassName('checklist__box')[2].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[3].value && document.getElementsByClassName('checklist__box')[3].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[3].value) !== '') && ((document.getElementsByClassName('checklist__box')[3].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[4].value && document.getElementsByClassName('checklist__box')[4].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[4].value) !== '') && ((document.getElementsByClassName('checklist__box')[4].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
   } else if ((document.getElementsByClassName('checklist__to-do-item')[5].value && document.getElementsByClassName('checklist__box')[5].checked) === false){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[6].value && document.getElementsByClassName('checklist__box')[6].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[6].value) !== '') && ((document.getElementsByClassName('checklist__box')[6].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[7].value && document.getElementsByClassName('checklist__box')[7].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[7].value) !== '') && ((document.getElementsByClassName('checklist__box')[7].checked) === false)){
       alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[8].value && document.getElementsByClassName('checklist__box')[8].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[8].value) !== '') && ((document.getElementsByClassName('checklist__box')[8].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
-  } else if ((document.getElementsByClassName('checklist__to-do-item')[9].value && document.getElementsByClassName('checklist__box')[9].checked) === false){
+  } else if (((document.getElementsByClassName('checklist__to-do-item')[9].value) !== '') && ((document.getElementsByClassName('checklist__box')[9].checked) === false)){
     alert("Oops! Did you finish your tasks yet?");
   } else {
     alert("Great work!")
     document.getElementById("checklist").reset();
   }
 };
-  
-
-
